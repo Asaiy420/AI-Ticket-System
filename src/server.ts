cb400e76813ca243049da9e6a3022b4347f4c0d2
Asaiy420 <kcaryan05@gmail.com>
2025-06-17 12:37:53 +0545

    refractor line

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
