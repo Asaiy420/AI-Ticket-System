9541650cb57ecfa0b8f8e58afd695f03ad787c4b
Asaiy420 <kcaryan05@gmail.com>
2025-06-17 12:42:31 +0545

    update controller logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 
 export const SignUp = async (req:Request, res:Response):Promise<void> => {
-    res.send("SignUp");
+    
 }
 
 export const Login = async (req:Request, res:Response):Promise<void> => {
