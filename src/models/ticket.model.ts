9ccd649bec959557bd8c560d4b42a5af2e82a6d2
Asaiy420 <kcaryan05@gmail.com>
2025-06-17 13:47:49 +0545

    add: model schemas

---
+++ src/models/ticket.model.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+interface ITicket extends mongoose.Document {
+  title: string;
+  description: string;
+  status: string;
+  createdBy: mongoose.Types.ObjectId;
+  assignedTo: mongoose.Types.ObjectId;
+  priority: string;
+  deadline: Date;
+  helpfulNotes: string;
+  realtedSkills: string[];
+}
+
+const ticketSchema = new mongoose.Schema<ITicket>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "WAITING",
+      enum: ["WAITING", "IN_PROGRESS", "COMPLETED"],
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+
+    priority: String,
+    deadline: Date,
+    helpfulNotes: String,
+    realtedSkills: [String],
+  },
+  { timestamps: true }
+);
+
+const Ticket = mongoose.model<ITicket>("Ticket", ticketSchema);
---
+++ src/models/user.model.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+
+interface IUser extends mongoose.Document {
+    email: string;
+    password: string;
+    role: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "moderator", "admin"],
+        skills: [String],
+
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+
+    
+}, {timestamps: true})
+
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
