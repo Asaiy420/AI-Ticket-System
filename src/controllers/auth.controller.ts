b0272d6d700ec1233ab2ed5bfd3fc3e54c708214
Asaiy420 <kcaryan05@gmail.com>
2025-06-18 12:12:10 +0545

    add: ( feat ) login logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -43,10 +43,11 @@ export const SignUp = async (req: Request, res: Response): Promise<void> => {
     const token = jwt.sign(
       { _id: user._id, role: user.role },
       process.env.JWT_SECRET as string,
-      { expiresIn: "1d" }
+      { expiresIn: "7d" }
     );
 
     res.status(201).json({
+      message: "SignUp Successful!",
       token,
       user: {
         _id: user._id,
@@ -62,7 +63,49 @@ export const SignUp = async (req: Request, res: Response): Promise<void> => {
 };
 
 export const Login = async (req: Request, res: Response): Promise<void> => {
-  res.send("Login");
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      res.status(400).json({ messaeg: "Both email and password are required" });
+      return;
+    }
+    // check if email exists
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(404).json({ message: "User not found please sign up first" });
+      return;
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      res.status(400).json({ error: "Invalid Credentials" });
+      return;
+    }
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      message: "Login Successful!",
+      token,
+      user: {
+        _id: user._id,
+        email: user.email,
+        role: user.role,
+        skills: user.skills,
+      },
+    });
+  } catch (error) {
+    console.log("Error in Login controller:", error);
+    res.status(200).json({ error: "Invalid Credentials" });
+  }
 };
 
 export const Logout = async (req: Request, res: Response): Promise<void> => {
