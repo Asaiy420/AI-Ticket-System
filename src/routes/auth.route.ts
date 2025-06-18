a5e0c8fa56578e8ee8db826b6edbdced092ac215
Asaiy420 <kcaryan05@gmail.com>
2025-06-18 13:27:00 +0545

    add: update route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
-import {SignUp, Login, Logout} from "../controllers/auth.controller.js";
+import {SignUp, Login, Logout, updateUser} from "../controllers/auth.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -7,5 +8,6 @@ const router = Router();
 router.post("/signUp", SignUp)
 router.post("/login", Login)
 router.post("/logout", Logout)
+router.put("/update-user", authenticate, updateUser);
 
-export default router;
+export default router;  
