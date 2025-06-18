8beef5d2069351677b24329c48de7eefb578dd60
Asaiy420 <kcaryan05@gmail.com>
2025-06-18 11:34:18 +0545

    add: signUp logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,52 @@
 import {Request, Response} from "express";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import { inngest } from "../inngest/client";
+import jwt from "jsonwebtoken";
 
 export const SignUp = async (req:Request, res:Response):Promise<void> => {
+    const {email, password, skills = []} = req.body;
+
+   try {
+     if (!email || !password){
+         res.status(400).json({message: "Both email and password are required"});
+         return;
+     }
+ 
+     // check if email already exists
+ 
+     const existingUser = await User.findOne({email});
+ 
+     if (existingUser){
+         res.status(400).json({message: "Email already exists"});
+         return;
+     }
+
+     const hashedPassword = await bcrypt.hash(password, 10);
+
+     const user = await User.create({
+        email,
+        password: hashedPassword,
+        skills,
+     })
+
+     // fire inngest events
+
+     await inngest.send({
+        name: "user/signUp",
+        data: {
+            email,
+        }
+     })
+
+     
+
+
+
+   } catch (error) {
     
+   }
+
 }
 
 export const Login = async (req:Request, res:Response):Promise<void> => {
