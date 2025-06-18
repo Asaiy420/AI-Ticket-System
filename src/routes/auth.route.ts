590590ebd1bd43867b2ce4946a3fef5a6ea07a08
Asaiy420 <kcaryan05@gmail.com>
2025-06-18 19:27:42 +0545

    add: fetch users route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import {Router} from "express";
-import {SignUp, Login, Logout, updateUser} from "../controllers/auth.controller.js";
+import {SignUp, Login, Logout, updateUser, getUsers} from "../controllers/auth.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 
+router.get("/user",authenticate,getUsers)
 router.post("/signUp", SignUp)
 router.post("/login", Login)
 router.post("/logout", Logout)
 router.put("/update-user", authenticate, updateUser);
 
-export default router;  
+export default router;      
