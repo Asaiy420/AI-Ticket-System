2b1259ea2dd721f23572a944d04ee5e44226405b
Asaiy420 <kcaryan05@gmail.com>
2025-06-18 19:27:52 +0545

    add: (feat) fetch user logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -170,3 +170,23 @@ export const updateUser = async (
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+export const getUsers = async (req:Request, res:Response):Promise<void> => {
+    try{
+      if(req.user && typeof req.user !== "string" && req.user.role){
+        if (req.user?.role !== "admin"){
+          res.status(403).json({message: "You are not authorized to perform this action"})
+          return
+        }
+      }
+
+      const user = await User.find().select("-password")
+      res.status(200).json(user)
+      return;
+
+    }catch(error){
+      console.error("Error fetching users", error);
+      res.status(500).json({message: "Internal server error"});
+    }
+}
