865a0d252dcd9854f3263c171b2a1a0b2ed2faa7
Asaiy420 <kcaryan05@gmail.com>
2025-06-18 11:53:14 +0545

    update: user schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,38 +1,39 @@
 import mongoose from "mongoose";
 
-
 interface IUser extends mongoose.Document {
-    email: string;
-    password: string;
-    role: string;
+  email: string;
+  password: string;
+  role: string;
+  skills: string[];
 }
 
-const userSchema = new mongoose.Schema<IUser>({
+const userSchema = new mongoose.Schema<IUser>(
+  {
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
     },
 
     role: {
-        type: String,
-        default: "user",
-        enum: ["user", "moderator", "admin"],
-        skills: [String],
-
+      type: String,
+      default: "user",
+      enum: ["user", "moderator", "admin"],
+      skills: [String],
     },
 
     password: {
-        type: String,
-        required: true,
-        minLength: 6,
+      type: String,
+      required: true,
+      minLength: 6,
     },
 
-    
-}, {timestamps: true})
-
+    skills: [String],
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model<IUser>("User", userSchema);
 
