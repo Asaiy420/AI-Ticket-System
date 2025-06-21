8dd270428e1757e948ead79a774e1e7b7ff05100
Asaiy420 <kcaryan05@gmail.com>
2025-06-21 11:23:02 +0545

    add: logic to fetch all tickets bsed on roles

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -44,3 +44,35 @@ export const createTicket = async (
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getAllTickets = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    let tickets: ITicket[] = [];
+
+    if (
+      req.user &&
+      typeof req.user !== "string" &&
+      (req.user.role === "admin" || req.user.role === "moderator")
+    ) {
+      tickets = await Ticket.find({})
+        .populate("assignedTo", ["email", "_id"])
+        .sort({ createdAt: -1 });
+    } else if (req.user && typeof req.user !== "string") {
+      tickets = await Ticket.find({ createdBy: req.user._id })
+        .select("title description status createdAt")
+        .sort({ createdAt: -1 });
+    }
+
+    res.status(200).json({
+      message: "Tickets fetched successfully",
+      tickets,
+    });
+    return;
+  } catch (error) {
+    console.error("Error fetching tickets:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
