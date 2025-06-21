66bd049c319d0813e2bec42074faf26d264872ec
Asaiy420 <kcaryan05@gmail.com>
2025-06-21 11:22:52 +0545

    add: (feat) getAllTicket route

--- src/routes/ticket.route.ts
+++ src/routes/ticket.route.ts
@@ -1,10 +1,10 @@
 import {Router} from "express";
-import {createTicket} from "../controllers/ticket.controller";
+import {createTicket, getAllTickets} from "../controllers/ticket.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-
+router.get("/all", authenticate, getAllTickets);
 router.post("/create",authenticate,createTicket);
 
 
