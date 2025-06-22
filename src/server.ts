718003fb6a70f9daeaad2ac0854c66c7325a0f57
Asaiy420 <kcaryan05@gmail.com>
2025-06-22 10:36:25 +0545

    add: inngest routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/DB.js";
 import ticketRoutes from "./routes/ticket.route.js";
+import { serve } from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onSignUp } from "./inngest/functions/on-signUp.js";
+import { onTicketCreate } from "./inngest/functions/on-ticket-create.js";
 
 const app = express();
 
@@ -12,6 +16,13 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/ticket", ticketRoutes);
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onSignUp, onTicketCreate],
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
